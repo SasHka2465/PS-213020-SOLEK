776785b152a86ddd08ca1cdb72a67392216a71cb
Diana001a <82045854+Diana001a@users.noreply.github.com>
2021-10-02 17:55:48 +0300

    ЧТо-то сделали

---
+++ PS-213020-SOLEK/PS-213020-SOLEK/Context/NoteContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PS_213020_SOLEK.Models;
+
+namespace PS_213020_SOLEK.Contexts
+{
+    public class NoteContext : DbContext
+    {
+        public NoteContext(DbContextOptions<NoteContext> option) : base(option)
+        {
+
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
